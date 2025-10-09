69f72be45b725014c549483c8776a89eb3d7e059
johnr <dohertyjr0@icloud.com>
2025-10-09 21:54:32 +0100

    created Payments list

---
+++ src/main/java/atu/ie/bankservice/PaymentServices.java
@@ -0,0 +1,20 @@
+package atu.ie.bankservice;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PaymentServices {
+    private final List<Payment> payments = new ArrayList<>();
+
+    public List<Payment> getPayments() {
+        return payments;
+    }
+
+    public Payment createPayment(Payment payment) {
+        payments.add(payment);
+        return payment;
+    }
+}
