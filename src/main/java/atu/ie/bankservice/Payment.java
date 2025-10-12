e12cc93df4a6a028dc3df25e076e0fc7df2f0b18
johnr <dohertyjr0@icloud.com>
2025-10-12 20:30:02 +0100

    Fully implemented CRUD design with dummy data

--- src/main/java/atu/ie/bankservice/Payment.java
+++ src/main/java/atu/ie/bankservice/Payment.java
@@ -1,10 +1,12 @@
 package atu.ie.bankservice;
 
 public class Payment {
-    private final String id;
-    private final double amount;
-    public final String tx;
-    public  final String rx;
+    private String id;
+    private double amount;
+    private String tx;
+    private String rx;
+
+    public Payment(){}
 
     public Payment(String id, double amount, String tx, String rx) {
         this.id = id;
@@ -16,16 +18,28 @@ public class Payment {
     public String getId() {
         return id;
     }
+    public void setId(String id){
+        this.id = id;
+    }
 
     public double getAmount() {
         return amount;
+    }
+    public void setAmount(double amount) {
+        this.amount = amount;
     }
 
     public String getTx() {
         return tx;
     }
+    public void setTx(String tx) {
+        this.tx = tx;
+    }
 
     public String getRx() {
         return rx;
+    }
+    public void setRx(String rx) {
+        this.rx = rx;
     }
 }
--- src/main/java/atu/ie/bankservice/PaymentServices.java
+++ src/main/java/atu/ie/bankservice/PaymentServices.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class PaymentServices {
@@ -14,20 +15,42 @@ public class PaymentServices {
 
     public PaymentServices(){
         payments = new ArrayList<>();
-        payments.add(new Payment("IE", 300, "Matthew", "John"));
+        payments.add(new Payment("0", 300, "Matthew", "John"));
+        payments.add(new Payment("1", 200, "Jack", "John"));
+        payments.add(new Payment("2", 400, "David", "John"));
     }
 
-    public List<Payment> getPayments(PaymentServices payment) {
+    public List<Payment> getPayments() {
         return payments;
     }
 
-    public List<Payment> createPayment(@RequestParam Payment payment) {
+    public List<Payment> createPayment(Payment payment) {
         payments.add(payment);
         return payments;
     }
 
-    public List<Payment> updatePayment(Payment payment) {
+    public Payment updatePayment(Payment payment) {
+        for(int i = 0; i < payments.size(); i++){
+            if(payments.get(i).getId().equals(payment.getId())){
+                payments.set(i, payment);
+                return payment;
+            }
+        }
         payments.add(payment);
-        return List.of(payment);
+        return payment;
+    }
+
+    public Payment getById(String id){
+        for(int i = 0; i < payments.size(); i++){
+            Payment p =  payments.get(i);
+            if(p.getId().equals(id)){
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public boolean deleteById(String id){
+        return payments.removeIf(p -> p.getId().equals(id));
     }
 }
