17d723a1ecbfb08e1e3e4703e4a6578a4bba953a
DAVIDPRENDERGAST-STU <g00405007@atu.ie>
2025-11-10 19:54:18 +0000

    added exception details class for error responses

---
+++ src/main/java/atu/ie/bankservice/account/errorHandling/ExceptionDetails.java
@@ -0,0 +1,30 @@
+package atu.ie.bankservice.account.errorHandling;
+
+public class ExceptionDetails {
+    private String fieldName;
+    private String fieldValue;
+
+    public ExceptionDetails() {}
+
+    public ExceptionDetails(String fieldName, String fieldValue) {
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public String getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(String fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+}
+
